e0b3ebe13aa773931f5deb813f6c158a8b85ef6a
Shawnm-cyber <shawnmathw14@gmail.com>
2025-04-07 20:38:57 -0400

    Widget setup and StatefulWidget declaration for app

--- lib/task_list_screen.dart
+++ lib/task_list_screen.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(const MyApp());
+
+class TaskListScreenextends StatefulWidget {
+  @override
+  _TaskListScreenState createState() => _TaskListScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class _TaskListScreenState {
+}
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firebase_crud_app/main.dart';
+import 'package:firebase_crud_app/task_list_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
